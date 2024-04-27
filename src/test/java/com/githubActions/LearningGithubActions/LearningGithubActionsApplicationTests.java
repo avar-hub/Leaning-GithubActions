13db6c2c3e1fe80d31a743d104477687ee0b596e
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 <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>
2024-04-27 19:11:49 +0530

    first commit

---
+++ src/test/java/com/githubActions/LearningGithubActions/LearningGithubActionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubActions.LearningGithubActions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningGithubActionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
